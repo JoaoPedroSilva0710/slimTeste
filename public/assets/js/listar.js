cbfba6f82a9097e441d9a02c4792c6a10e866bb1
João Pedro <joaosilva071003@gmail.com>
2025-01-15 14:05:07 -0300

    Correção de bugs

--- public/assets/js/listar.js
+++ public/assets/js/listar.js
@@ -1,11 +1,26 @@
-$(document).ready( function () {
-    $('#tablePacientes').DataTable();
-} );
 
-let response = await fetch('/pacientes');
-let obj = response.json();
-
-new DataTable('#tablePacientes', {
-    "ajax": obj
+$(
+async() => 
+{
+    let response = await fetch('/pacientes');
+    let obj = await response.json();
+    console.log(obj);
+    new DataTable('#tablePacientes', {
+        data: await obj,
+        columns: [
+            { title: "id" },
+            { title: "nome" },
+            { title: "data_nascimento" },
+            { title: "sexo" },
+            { title: "nome_mae" },
+            { title: "email" },
+            { title: "cpf" },
+            { title: "cep" },
+            { title: "nome_rua" },
+            { title: "numero_casa" },
+            { title: "bairro" },
+            { title: "uf" },
+            { title: "ativo" }
+        ]
 });
-
+});
