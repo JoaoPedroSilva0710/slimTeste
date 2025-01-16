20004ae06f0d64fc168ea777c20667bfa6831ed1
João Pedro <joaosilva071003@gmail.com>
2025-01-16 13:00:01 -0300

    Adicionado parte do front e encontrado bug para pegar o dataset-id

--- public/assets/js/listar.js
+++ public/assets/js/listar.js
@@ -1,26 +1,63 @@
-
 $(
 async() => 
 {
+    await constructDataTable();
+
+    
+});
+
+const fetchPacients = async() => 
+{
     let response = await fetch('/pacientes');
     let obj = await response.json();
-    console.log(obj);
-    new DataTable('#tablePacientes', {
-        data: await obj,
+    let data = await obj.data;
+
+    return data;
+
+}
+
+
+const constructDataTable = async() => {
+    let data = fetchPacients();
+
+    let table = new DataTable('#tablePacientes', {
+        data: await data,
         columns: [
-            { title: "id" },
-            { title: "nome" },
-            { title: "data_nascimento" },
-            { title: "sexo" },
-            { title: "nome_mae" },
-            { title: "email" },
-            { title: "cpf" },
-            { title: "cep" },
-            { title: "nome_rua" },
-            { title: "numero_casa" },
-            { title: "bairro" },
-            { title: "uf" },
-            { title: "ativo" }
+            { data: "nome" },
+            { data: "data_nascimento" },
+            { data: "sexo" },
+            { data: "nome_mae" },
+            { data: "email" },
+            { data: "cpf" },
+            { data: "cep" },
+            { data: "nome_rua" },
+            { data: "numero_casa" },
+            { data: "bairro" },
+            { data: "uf" },
+            { data: "ativo" },
+            { render: function(data, type, row) {
+             return `<div class="divButtonClass">
+                        <button class="delete-btn" data-id="${row.id}" id="btn-edit-pacient">
+                            <img src="/assets/imgs/editBtnImg.png" class="img-fluid">
+                        </button>
+                        <button class="edit-btn" data-id="${row.id}" id="btn-del-pacient">
+                            <img src="/assets/imgs/deleteBtnImg.png" class="img-fluid">
+                        </button>
+                    </div>
+                    `;
+            }
+        }
         ]
 });
-});
+
+    return table;
+
+}
+
+
+$("body").on("click", "#btn-edit-pacient", e => {
+        const id = e.target.dataset.id;
+        console.log(id);
+}
+
+)
--- src/Domain/Paciente/PacienteRepository.php
+++ src/Domain/Paciente/PacienteRepository.php
@@ -21,19 +21,18 @@ class PacienteRepository implements PacienteRepositoryInterface
      */
     public function findAll(): array
     {
-        $query = "SELECT * FROM usuarios";
+        try {
+
+        $query = "SELECT * FROM usuarios where ativo = TRUE";
         $stmt = $this->sql->prepare($query);
         $stmt->execute();
         $resp = $stmt->fetchAll(SQL::FETCH_ASSOC);
         
         return $resp;
-        // return[
-        //     Paciente::create(1, 'João', '01-02-2003', 'M', 'Lilian', 'joaosilva@gmail.com', '12345678912', '03343010', 'Rua dos Alfeneiros', '10', 'bairro do trabalho', 'SP', TRUE),
-        //     Paciente::create(2, 'Pedro', '01-02-2003', 'M', 'Lilian', 'pedrosilva@gmail.com', '123.456.789-13', '03343010', 'Rua dos Alfeneiros', '10', 'bairro do trabalho', 'SP', TRUE),
-        //     Paciente::create(3, 'Lucas', '01-02-2003', 'M', 'Lilian', 'lucasrodrigues@gmail.com', '22345678912', '03343010', 'Rua dos Alfeneiros', '10', 'bairro do trabalho', 'SP', TRUE),
-        //     Paciente::create(4, 'Giovani', '01-02-2003', 'M', 'Lilian', 'gurudoexcel@gmail.com', '32345678912', '03343010', 'Rua dos Alfeneiros', '10', 'bairro do trabalho', 'SP', TRUE),
-        //     Paciente::create(5, 'Tiago', '01-02-2003', 'M', 'Lilian', 'tiagoletieri@gmail.com', '42345678912', '03343010', 'Rua dos Alfeneiros', '10', 'bairro do trabalho', 'SP', TRUE)
-        // ];
+
+        } catch(Exception $e) {
+            die($e->getMessage());
+        }
     }
 
     /**
@@ -43,13 +42,14 @@ class PacienteRepository implements PacienteRepositoryInterface
     {
         try{
         
-        $query = "SELECT * FROM usuarios where id = :id";
+        $query = "SELECT * FROM usuarios WHERE id = :id AND ativo = TRUE";
         $stmt = $this->sql->prepare($query);
         $stmt->bindParam(":id", $id);
         $stmt->execute();
         $resp = $stmt->fetchAll(SQL::FETCH_ASSOC);
 
         return $resp;
+        
         } catch(Exception $e){
             die($e->getMessage());
 
